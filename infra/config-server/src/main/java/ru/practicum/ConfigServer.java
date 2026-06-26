1ce82bff6aeffd403ffd9a0f0d1afc4b73f25b33
Nikulshina Polina <nikulschina-p@yandex.ru>
2026-06-26 13:03:33 +0400

    исправление директорий

--- infra/config-server/src/main/java/ru/practicum/ConfigServer.java
+++ infra/config-server/src/main/java/ru/practicum/ConfigServer.java
@@ -1,12 +1,10 @@
-package ru.practicum.config_server;
+package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServer {
     public static void main(String[] args) {
--- infra/gateway-server/src/main/java/ru/practicum/Gateway.java
+++ infra/gateway-server/src/main/java/ru/practicum/Gateway.java
@@ -1,4 +1,4 @@
-package ru.practicum.gateway;
+package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
