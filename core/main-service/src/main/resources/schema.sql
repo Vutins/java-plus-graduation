d5a692a87e28c10e04e7bdf4f5b5e2e73c958472
Nikulshina Polina <nikulschina-p@yandex.ru>
2026-07-02 11:05:07 +0400

    исправление базы данных main-service

--- core/main-service/src/main/resources/schema.sql
+++ core/main-service/src/main/resources/schema.sql
@@ -23,7 +23,6 @@ CREATE TABLE IF NOT EXISTS events (
     category_id BIGINT NOT NULL,
     created_on TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     initiator_id BIGINT NOT NULL,
-    location_id BIGINT NOT NULL,
     lat FLOAT(4) NOT NULL,
     lon FLOAT(4) NOT NULL,
     event_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
