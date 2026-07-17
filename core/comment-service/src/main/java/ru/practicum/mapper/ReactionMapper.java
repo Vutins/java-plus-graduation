2163717c3807248a519798a4799256682be265c6
Nikulshina Polina <nikulschina-p@yandex.ru>
2026-07-17 18:23:35 +0400

    изменила ReactionMapper

--- core/comment-service/src/main/java/ru/practicum/mapper/ReactionMapper.java
+++ core/comment-service/src/main/java/ru/practicum/mapper/ReactionMapper.java
@@ -9,9 +9,9 @@ import ru.practicum.user.dto.UserShortDto;
 @Mapper(componentModel = "spring", uses = {CommentMapper.class})
 public interface ReactionMapper {
 
-    @Mapping(source = "comment", target = "commentResponseDto")
+    @Mapping(source = "reaction.comment", target = "commentResponseDto")
     @Mapping(source = "evaluator", target = "evaluator")
-    @Mapping(source = "created", target = "created", dateFormat = "yyyy-MM-dd HH:mm:ss")
-    @Mapping(source = "updated", target = "updated", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @Mapping(source = "reaction.created", target = "created", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @Mapping(source = "reaction.updated", target = "updated", dateFormat = "yyyy-MM-dd HH:mm:ss")
     ReactionResponseDto toReactionResponseDto(Reaction reaction, UserShortDto evaluator);
 }
