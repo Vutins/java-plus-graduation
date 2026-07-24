e1ac1280864f255435f4635af54c271a12c07c98
Nikulshina Polina <nikulschina-p@yandex.ru>
2026-07-24 22:26:52 +0400

    checkStyle

--- core/request-service/src/main/java/ru/practicum/explore_with_me/request/mapper/ParticipationRequestMapper.java
+++ core/request-service/src/main/java/ru/practicum/explore_with_me/request/mapper/ParticipationRequestMapper.java
@@ -7,6 +7,7 @@ import ru.practicum.explore_with_me.request.dao.ParticipationRequest;
 
 @Mapper(componentModel = "spring")
 public interface ParticipationRequestMapper {
+
     @Mapping(target = "event", source = "eventId")
     @Mapping(target = "requester", source = "requesterId")
     ParticipationRequestDto toParticipationRequestDto(ParticipationRequest participationRequest);
