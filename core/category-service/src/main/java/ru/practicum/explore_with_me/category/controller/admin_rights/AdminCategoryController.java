6bbd96b20f663d163637b6bd5907b4896801d4be
Nikulshina Polina <nikulschina-p@yandex.ru>
2026-07-24 21:54:40 +0400

    checkStyle

--- core/category-service/src/main/java/ru/practicum/explore_with_me/category/controller/admin_rights/AdminCategoryController.java
+++ core/category-service/src/main/java/ru/practicum/explore_with_me/category/controller/admin_rights/AdminCategoryController.java
@@ -19,6 +19,7 @@ import java.util.List;
 @RequestMapping("/admin/categories")
 @RequiredArgsConstructor
 public class AdminCategoryController {
+
     private final CategoryService categoryService;
 
     @PostMapping
